077735f8f45016e6f1ef9fa9b72db16627c75bea
zergkim <zergkim06@gmail.com>
2021-04-24 12:44:12 +0900

    개꿀

--- new.js
+++ new.js
@@ -3,7 +3,8 @@ const url = "mongodb+srv://zergkim:kimsh060525@cluster0.55ags.mongodb.net/myFirs
 const client = new MongoClient(url, { useUnifiedTopology: true });
 const crypto = require('crypto');
 const idar= []
-
+const {splite}=require("./new_js/splite")
+const {postthedata}=require("./new_js/splite")
 let postobj={}
 let dbobj = {
 }
@@ -23,25 +24,8 @@ const filelist=[];
 const juso = "mongodb+srv://zergkim:<password>@cluster0.55ags.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const hls = require("hls-server")
 const fs=require('fs');
-const ffmpeg = require('fluent-ffmpeg');
-const ffmpegInstaller=require("@ffmpeg-installer/ffmpeg");
-const { Socket } = require("dgram");
-ffmpeg.setFfmpegPath(ffmpegInstaller.path)
-const splite = (name,d)=>{
-    return new Promise((res,rej)=>{
-        ffmpeg('./savefiles/'+name+'.'+d,{timeout:432000}).addOptions([
-            '-profile:v baseline',
-            '-level 3.0',
-            '-start_number 0',
-            '-hls_time 10',//10초 단위임
-            '-hls_list_size 0',
-            '-f hls'
-        ]).output("videos/"+name+'.m3u8').on('end',()=>{
-            res("end")
-        }).run() 
-    })
-    
-}
+
+
 app.use('/img',express.static('./img'))
 app.use('/node_modules',express.static('./node_modules'))
 app.use(express.raw({limit:'1gb'}))//이거 꼭설정 해야함
@@ -51,6 +35,7 @@ app.get("/postid",(req,res)=>{
     const secret = 'kopqqqwi!!';
 
     const hashed = crypto.createHmac('sha256', secret).update(password).digest('hex');
+    postobj[hashed]=true;
     res.send(hashed)
 })
 app.get("/userip",(req,res)=>{
@@ -71,32 +56,44 @@ app.get("/main/filelist",async(req,res)=>{
 })
 
 app.post("/videopost",async(req,res)=>{
-    const obj = JSON.parse(JSON.stringify(req.query))
-   
     
+    const obj = req.query
+    console.log("erwre")
+    if(!postobj[obj.name]){
+        console.log("노답qffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")
+        res.send("요청이 잘못되었습니다")
+    }
+    console.log("hi")
     postobj[obj.name]=new Array()
     postobj[obj.name].push(req.body)
     
-    
+    console.log(req.query.name)
     res.send("ok")
 })
 app.post("/imgpost",async(req,res)=>{
     const obj = req.query
-    
+    if(!postobj[obj.name]){
+        res.send("요청이 잘못되었습니다")
+    }
     postobj[obj.name].push(req.body)
-    
-    
+    console.log(req.query.name)
+    console.log("fwef:",postobj[obj.name])
     res.send("ok")
 })
 app.post("/objpost",async(req,res)=>{
     let obj = req.body
+    obj.ip = req.socket.remoteAddress
     const nameqe = req.query.name
+    console.log("df:",postobj[nameqe])
+    if(!postobj[nameqe]){
+        res.send("요청이 잘못되었습니다")
+    }
     postobj[nameqe].push(obj)
     console.log("\n",postobj[nameqe])
     
     await post_func(postobj[nameqe])
     delete postobj[nameqe];
-    res.send("goood")
+    res.send("good")
     
 })
 const server= app.listen(3000,async()=>{
@@ -160,29 +157,3 @@ function post_func(obj){
     postthedata(videod,imgd,vobjd)
 }
 
-function postthedata(vdata,idata,dobj){
-    return new Promise(async(res,rej)=>{
-        let d = async function(){
-            console.log(dobj)
-            let typename = dobj.typeofv
-            let typename2 = dobj.typeofi
-            const upobj = await dbobj.df.insertOne(dobj)
-            const videoid = upobj.insertedId;
-            const file_name=videoid+"."+typename
-            const file_name2=videoid+"."+typename2
-            await fs.promises.writeFile(`savefiles/${file_name}`, vdata);
-            fs.promises.writeFile(`img/${file_name2}`,idata)
-            await splite(videoid,typename)
-            console.log("성공")
-            res("성공")
-        }
-        try{
-            d()
-        }catch(e){
-            console.log("실패")
-            rej("실패")
-        }
-        
-        
-    })
-}
---
+++ new_js/splite.js
@@ -0,0 +1,60 @@
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegInstaller=require("@ffmpeg-installer/ffmpeg");
+const {MongoClient} = require("mongodb");
+const url = "mongodb+srv://zergkim:kimsh060525@cluster0.55ags.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(url, { useUnifiedTopology: true });
+ffmpeg.setFfmpegPath(ffmpegInstaller.path)
+const fs = require('fs')
+let dbobj = {}
+client.connect(async e=>{
+    if(e){
+        console.error(e)
+    }
+    console.log('connection_complete!')
+    dbobj["db"]=client.db('streamingdata')
+    dbobj["df"]=dbobj["db"].collection("videodata")
+    
+})
+const splite = (name,d)=>{
+    return new Promise((res,rej)=>{
+        ffmpeg('./savefiles/'+name+'.'+d,{timeout:432000}).addOptions([
+            '-profile:v baseline',
+            '-level 3.0',
+            '-start_number 0',
+            '-hls_time 10',//10초 단위임
+            '-hls_list_size 0',
+            '-f hls'
+        ]).output("videos/"+name+'.m3u8').on('end',()=>{
+            res("end")
+        }).run() 
+    })
+    
+}
+function postthedata(vdata,idata,dobj){
+    
+    return new Promise(async(res,rej)=>{
+        let d = async function(){
+            console.log(dobj)
+            let typename = dobj.typeofv
+            let typename2 = dobj.typeofi
+            const upobj = await dbobj.df.insertOne(dobj)
+            const videoid = upobj.insertedId;
+            const file_name=videoid+"."+typename
+            const file_name2=videoid+"."+typename2
+            await fs.promises.writeFile(`savefiles/${file_name}`, vdata);
+            fs.promises.writeFile(`img/${file_name2}`,idata)
+            await splite(videoid,typename)
+            console.log("성공")
+            res("성공")
+        }
+        try{
+            d()
+        }catch(e){
+            console.log("실패")
+            rej("실패")
+        }
+        
+        
+    })
+}
+module.exports={splite, postthedata}
